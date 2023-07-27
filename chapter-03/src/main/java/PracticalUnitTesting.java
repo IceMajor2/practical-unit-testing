05c56f65cda9469de2eb82c4956eaae175a95f1b
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-27 14:19:59 +0200

    Refactor reverse method

--- chapter-03/src/main/java/PracticalUnitTesting.java
+++ chapter-03/src/main/java/PracticalUnitTesting.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class PracticalUnitTesting {
 
 	public static void main(String[] args) {
@@ -8,13 +5,9 @@ public class PracticalUnitTesting {
 	}
 
 	public static String reverse(String s) {
-		List<String> tempArray = new ArrayList<String>(s.length());
-		for (int i = 0; i < s.length(); i++) {
-			tempArray.add(s.substring(i, i + 1));
-		}
 		StringBuilder reversedString = new StringBuilder(s.length());
-		for (int i = tempArray.size() - 1; i >= 0; i--) {
-			reversedString.append(tempArray.get(i));
+		for (int i = s.length() - 1; i >= 0; i--) {
+			reversedString.append(s.charAt(i));
 		}
 		return reversedString.toString();
 	}
