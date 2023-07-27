a4c9b80f107a74863d63bf0cc262090aad85dbaf
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-27 14:37:50 +0200

    Create empty map before each test

--- chapter-03/src/test/java/HashMapTest.java
+++ chapter-03/src/test/java/HashMapTest.java
@@ -16,11 +16,11 @@ public class HashMapTest {
 	@BeforeEach
 	void setUp() {
 		data = new HashMap<>();
-		data.put(1, "One");
-		data.put(2, "Two");
-		data.put(3, "Three");
-		data.put(4, "Four");
-		data.put(5, "Five");
+//		data.put(1, "One");
+//		data.put(2, "Two");
+//		data.put(3, "Three");
+//		data.put(4, "Four");
+//		data.put(5, "Five");
 	}
 
 	@ParameterizedTest
@@ -32,19 +32,21 @@ public class HashMapTest {
 			"5, Five"
 	})
 	void getReturnsKeyTest(int key, String value) {
+		data.put(key, value);
 		String actual = data.get(key);
 		assertThat(actual).isEqualTo(value);
 	}
 
 	@ParameterizedTest
 	@CsvSource({
-			"1, Jeden",
-			"2, Dwa",
-			"3, Trzy",
-			"4, Cztery",
-			"5, Piec"
+			"1, One, Jeden",
+			"2, Two, Dwa",
+			"3, Three, Trzy",
+			"4, Four, Cztery",
+			"5, Five, Piec"
 	})
-	void putReplacesPreviousValueTest(int key, String replacement) {
+	void putReplacesPreviousValueTest(int key, String original, String replacement) {
+		data.put(key, original);
 		String previous = data.get(key);
 		assertThat(previous).isNotNull();
 
@@ -56,6 +58,8 @@ public class HashMapTest {
 	@ParameterizedTest
 	@ValueSource(ints = {1, 2, 3, 4, 5})
 	void clearRemovesAllMappingsTest(int key) {
+		data.put(1000, "Not");
+		data.put(0, "Empty");
 		assertThat(data).isNotEmpty();
 		data.clear();
 		assertThat(data).isEmpty();
