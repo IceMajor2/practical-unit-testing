5c62213dbd4f31ec6b77db240378dedcabf06f48
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-27 14:13:04 +0200

    Change test names; write a comment (explanation); write new test

--- chapter-03/src/test/java/PracticalUnitTestingTest.java
+++ chapter-03/src/test/java/PracticalUnitTestingTest.java
@@ -14,7 +14,7 @@ public class PracticalUnitTestingTest {
 			"kajak, kajak",
 			"morela, alerom"
 	})
-	void whenStringIsWord_thenReturnReversedTest(String input, String expected) {
+	void whenStringIsWord_thenOKTest(String input, String expected) {
 		String actual = PracticalUnitTesting.reverse(input);
 		assertThat(actual).isNotNull();
 		assertThat(actual).isEqualTo(expected);
@@ -25,7 +25,7 @@ public class PracticalUnitTestingTest {
 			"ala ma kota, atok am ala",
 			"practical unit testing, gnitset tinu lacitcarp"
 	})
-	void whenStringIsSentence_thenReturnReversedTest(String input, String expected) {
+	void whenStringIsSentence_thenOKTest(String input, String expected) {
 		String actual = PracticalUnitTesting.reverse(input);
 		assertThat(actual).isNotNull();
 		assertThat(actual).isEqualTo(expected);
@@ -37,10 +37,26 @@ public class PracticalUnitTestingTest {
 			"'   ', '   '",
 			"' \t \n', '\n \t '"
 	})
-	void whenStringIsBlank_thenReverseShouldNotChangeTest(String input, String expected) {
+	void whenStringIsBlank_thenOKTest(String input, String expected) {
 		String actual = PracticalUnitTesting.reverse(input);
 		assertThat(input).isNotNull();
+		// I'm allowing here for *not* reversing the input string,
+		// in case it's blank. Thus, the actual output may be the same as input.
 		assertThat(actual).isIn(expected, input);
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+			">2?ska, aks?2>",
+			"-!~k, k~!-",
+			".:=||)87^], ]^78)||=:.",
+			"!$//*123456789, 987654321*//$!",
+			"\\\\//)(*&^ %$#@!, !@#$% ^&*()//\\\\"
+	})
+	void whenStringContainsSpecialCharactersAndDigits_thenOKTest(String input, String expected) {
+		String actual = PracticalUnitTesting.reverse(input);
+		assertThat(actual).isNotNull();
+		assertThat(actual).isEqualTo(expected);
 	}
 
 	@Test
