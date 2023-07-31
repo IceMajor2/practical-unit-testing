19aae7d79017bb3ac3081e2325f970b478bd32cb
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-31 13:13:57 +0200

    Check for digits in password

--- chapter-04/src/main/java/PasswordValidator.java
+++ chapter-04/src/main/java/PasswordValidator.java
@@ -1,6 +1,8 @@
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class PasswordValidator {
 
 	private static final int MIN_CHARS = 12;
@@ -10,16 +12,20 @@ public class PasswordValidator {
 	private static final int MIN_DIGITS = 1;
 
 	private static final String SYMBOLS = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
+	private static final String DIGITS = "0123456789";
 
 	public static boolean isValid(String password) {
-		List<Character> passwordChars = stringToCharList(password);
-		if(passwordChars.size() < MIN_CHARS) {
+		//List<Character> passwordChars = stringToCharList(password);
+		if(password.length() < MIN_CHARS) {
+			return false;
+		}
+		if(!StringUtils.containsAny(password, DIGITS)) {
 			return false;
 		}
 		return true;
 	}
 
-	private static List<Character> stringToCharList(String string) {
-		return string.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
-	}
+//	private static List<Character> stringToCharList(String string) {
+//		return string.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+//	}
 }
