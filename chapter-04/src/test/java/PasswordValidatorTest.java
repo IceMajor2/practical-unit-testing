6f8cf0f3eccc96ff84a3f40d085ca12908395754
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-31 13:27:36 +0200

    Write a test that provides passwords violating multiple constraints

--- chapter-04/src/test/java/PasswordValidatorTest.java
+++ chapter-04/src/test/java/PasswordValidatorTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PasswordValidatorTest {
 
 	@ParameterizedTest
-	@ValueSource(strings = {"ostYb_hB7@8#", "mks1\\l__HYHA", "76&JPa_krL01+"})
+	@ValueSource(strings = { "ostYb_hB7@8#", "mks1\\l__HYHA", "76&JPa_krL01+" })
 	void shouldReturnValidOnStrongPasswordTest(String password) {
 		assertThat(PasswordValidator.isValid(password))
 				.withFailMessage("Validation should return true as password is strong enough")
@@ -14,7 +14,7 @@ class PasswordValidatorTest {
 	}
 
 	@ParameterizedTest
-	@ValueSource(strings = {"ospaLL_][/hJU", "r_j#y&YByE-n_aHY", "gX=h?e#C#*en"})
+	@ValueSource(strings = { "ospaLL_][/hJU", "r_j#y&YByE-n_aHY", "gX=h?e#C#*en" })
 	void shouldReturnInvalidOnPasswordNoDigitsTest(String password) {
 		assertThat(PasswordValidator.isValid(password))
 				.withFailMessage("Validation should be false as everything but digits is ok")
@@ -22,7 +22,7 @@ class PasswordValidatorTest {
 	}
 
 	@ParameterizedTest
-	@ValueSource(strings = {"&n2'$xvg<c{m", "4d)@,a_k<r3>?^zxe^", "4>*=)q3-q/y:$w"})
+	@ValueSource(strings = { "&n2'$xvg<c{m", "4d)@,a_k<r3>?^zxe^", "4>*=)q3-q/y:$w" })
 	void shouldReturnInvalidOnPasswordNoUppercaseTest(String password) {
 		assertThat(PasswordValidator.isValid(password))
 				.withFailMessage("Validation should be false as everything but uppercase letter count is ok")
@@ -30,7 +30,7 @@ class PasswordValidatorTest {
 	}
 
 	@ParameterizedTest
-	@ValueSource(strings = {"CG'+Y.P4&NS$2]", ">$'6D77-#{M5&[KHX}+", "+-_4H+>RWP35"})
+	@ValueSource(strings = { "CG'+Y.P4&NS$2]", ">$'6D77-#{M5&[KHX}+", "+-_4H+>RWP35" })
 	void shouldReturnInvalidOnPasswordNoLowercaseTest(String password) {
 		assertThat(PasswordValidator.isValid(password))
 				.withFailMessage("Validation should be false as everything but lowercase letter count is ok")
@@ -38,7 +38,7 @@ class PasswordValidatorTest {
 	}
 
 	@ParameterizedTest
-	@ValueSource(strings = {"7nYvL5vK3ddK", "Chu6zHuyqSd8V", "mKzCWgYarhHX2vLHvm5nwa"})
+	@ValueSource(strings = { "7nYvL5vK3ddK", "Chu6zHuyqSd8V", "mKzCWgYarhHX2vLHvm5nwa" })
 	void shouldReturnInvalidOnPasswordNoSymbolTest(String password) {
 		assertThat(PasswordValidator.isValid(password))
 				.withFailMessage("Validation should be false as everything but symbol presence is ok")
@@ -46,10 +46,17 @@ class PasswordValidatorTest {
 	}
 
 	@ParameterizedTest
-	@ValueSource(strings = {"eT%P#V'vh6^", "2p:,Du_F", "E7:x7j"})
+	@ValueSource(strings = { "eT%P#V'vh6^", "2p:,Du_F", "E7:x7j" })
 	void shouldReturnInvalidOnPasswordTooShortTest(String password) {
 		assertThat(PasswordValidator.isValid(password))
 				.withFailMessage("Validation should be false as everything but password length is ok")
 				.isFalse();
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "h3thzt", "vavzxtv828c895zgb", "#O]TIG*CH*_]SHS", "``?;++\\,#^_",
+			"~{/?(`$@}59|-5`:4+4284(}[=.\\", "u.ybv\"t<+c^]", ";GAR%,?\\" })
+	void shouldReturnInvalidOnPasswordFailingMultipleConstraintsTest(String password) {
+		assertThat(PasswordValidator.isValid(password)).isFalse();
 	}
 }
