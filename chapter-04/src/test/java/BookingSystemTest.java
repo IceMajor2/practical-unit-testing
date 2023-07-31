9834255fb4211f152db21bda6a66fd1f9332d224
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-31 15:20:30 +0200

    Write new tests

--- chapter-04/src/test/java/BookingSystemTest.java
+++ chapter-04/src/test/java/BookingSystemTest.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,7 @@ class BookingSystemTest {
 
 	private BookingSystem SUT;
 
-	private List<Integer> bookings = List.of(
+	private Set<Integer> bookings = Set.of(
 			8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20
 	);
 
@@ -24,15 +25,15 @@ class BookingSystemTest {
 
 	@Test
 	void getBookedHours_shouldReturnListOfBookedHoursTest() {
-		List<Integer> actualBookings = SUT.getBookings();
+		Set<Integer> actualBookings = SUT.getBookings();
 		assertThat(actualBookings).hasSameElementsAs(bookings);
 	}
 
 	@ParameterizedTest
-	@ValueSource(ints = {1, 11, 21})
+	@ValueSource(ints = { 1, 11, 21 })
 	void whenBookingSingleFreeHour_thenOkTest(int hour) {
 		assertThat(SUT.getBookings()).doesNotContain(hour);
-		SUT.book(hour);
+		assertThat(SUT.book(hour)).isTrue();
 		assertThat(SUT.getBookings()).contains(hour);
 	}
 
@@ -45,17 +46,29 @@ class BookingSystemTest {
 	void whenBookingMultipleFreeHours_thenOkTest(int from, int to, String newHoursStr) {
 		Integer[] newHours = extractIntegers(newHoursStr);
 		assertThat(SUT.getBookings()).doesNotContain(newHours);
-		SUT.book(from, to);
+		assertThat(SUT.book(from, to)).isTrue();
 		assertThat(SUT.getBookings()).contains(newHours);
 	}
 
 	@ParameterizedTest
-	@ValueSource(ints = {15, 20, 8, 11})
+	@ValueSource(ints = { 15, 20, 8, 12 })
 	void whenBookingTakenHour_returnFalseTest(int hour) {
 		boolean result = SUT.book(hour);
 		assertThat(result)
 				.withFailMessage("Already-booked hour should not be allowed to be booked again")
 				.isFalse();
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+			"6, 8",
+			"10, 14",
+			"11, 12",
+			"19, 20"
+	})
+	void whenMultipleBookingTakenHour_returnFalseTest(int from, int to) {
+		assertThat(SUT.book(from, to)).isFalse();
+		assertThat(SUT.getBookings()).isEqualTo(bookings);
 	}
 
 	private Integer[] extractIntegers(String string) {
