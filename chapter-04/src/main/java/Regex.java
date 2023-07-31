1cdd5bfafbfa5956c2086d839230b8ce6098748f
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-31 13:51:45 +0200

    Pass test

--- chapter-04/src/main/java/Regex.java
+++ chapter-04/src/main/java/Regex.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex {
 
+	private static final String THREE_OR_MORE_DIGIT_REGEX = "\\d{3,}";
+
 	public static String digitSequence(String input) {
-		return null;
+		Pattern pattern = Pattern.compile(THREE_OR_MORE_DIGIT_REGEX);
+		Matcher matcher = pattern.matcher(input);
+
+		List<String> sequences = new ArrayList<>();
+		StringBuilder sb = new StringBuilder("");
+		while(matcher.find()) {
+			sequences.add(matcher.group());
+		}
+		return String.join(", ", sequences);
 	}
 }
