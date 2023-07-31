abd59d5f21b31d390467c392449d7bf1cb11c8dd
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-31 14:06:37 +0200

    Create a field 'bookings' and add getter and setter for it

--- chapter-04/src/main/java/BookingSystem.java
+++ chapter-04/src/main/java/BookingSystem.java
@@ -1,13 +1,14 @@
-import java.util.Collections;
 import java.util.List;
 
 public class BookingSystem {
+
+	private List<Integer> bookings;
 
 	public void book(int from, int to) {
 
 	}
 
 	public List<Integer> getBookings() {
-		return Collections.emptyList();
+		return this.bookings;
 	}
 }
