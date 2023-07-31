aeaa1d8e013a94dac69031c6da532a3c4c8f27ba
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-31 13:05:13 +0200

    Parameterize test

--- chapter-04/src/test/java/PasswordValidatorTest.java
+++ chapter-04/src/test/java/PasswordValidatorTest.java
@@ -1,12 +1,13 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PasswordValidatorTest {
 
-	@Test
-	void shouldReturnValidOnStrongPasswordTest() {
-		String password = "ostYb_hB7@8#";
+	@ParameterizedTest
+	@ValueSource(strings = {"ostYb_hB7@8#", "mks1\\l__HYHA", "76&JPa_krL01+"})
+	void shouldReturnValidOnStrongPasswordTest(String password) {
 		assertThat(PasswordValidator.isValid(password))
 				.withFailMessage("Validation should return true as password is strong enough")
 				.isTrue();
