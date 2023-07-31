309979a7ec9c75b6bb627617222cc65f957d683f
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-31 14:08:57 +0200

    Revert "Create a field 'bookings' and add getter and setter for it"
    This reverts commit abd59d5f21b31d390467c392449d7bf1cb11c8dd.

--- chapter-04/src/main/java/BookingSystem.java
+++ chapter-04/src/main/java/BookingSystem.java
@@ -1,14 +1,13 @@
+import java.util.Collections;
 import java.util.List;
 
 public class BookingSystem {
-
-	private List<Integer> bookings;
 
 	public void book(int from, int to) {
 
 	}
 
 	public List<Integer> getBookings() {
-		return this.bookings;
+		return Collections.emptyList();
 	}
 }
