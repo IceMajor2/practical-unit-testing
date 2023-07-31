2a120ab3fc3351390d9007505a081aa8fb27740e
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-31 14:15:34 +0200

    Slight change to setup SUT data; write new test that checks booking of multiple hours

--- chapter-04/src/test/java/BookingSystemTest.java
+++ chapter-04/src/test/java/BookingSystemTest.java
@@ -2,14 +2,17 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 class BookingSystemTest {
 
 	private BookingSystem SUT;
+
 	private List<Integer> bookings = List.of(
-			8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21
+			8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20
 	);
 
 	@BeforeEach
@@ -22,5 +25,17 @@ class BookingSystemTest {
 	void getBookedHours_shouldReturnListOfBookedHoursTest() {
 		List<Integer> actualBookings = SUT.getBookings();
 		assertThat(actualBookings).containsExactlyElementsOf(bookings);
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+			"21, 22",
+			"23, 1",
+			"2, 3"
+	})
+	void whenBookingMultipleFreeHours_thenOkTest(int from, int to) {
+		assertThat(SUT.getBookings()).doesNotContain(21, 22, 23, 24, 1, 2, 3);
+		SUT.book(from, to);
+		assertThat(SUT.getBookings()).contains(21, 22, 23, 24, 1, 2, 3);
 	}
 }
