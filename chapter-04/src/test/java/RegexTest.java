a61133cc3b86c9c389b280643b848b4c00986ca5
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-31 13:54:20 +0200

    Add new test cases

--- chapter-04/src/test/java/RegexTest.java
+++ chapter-04/src/test/java/RegexTest.java
@@ -9,7 +9,10 @@ class RegexTest {
 	@CsvSource(value = {
 			"abc 12; ''",
 			"cdefg 345 12bb23; 345",
-			"cdefg 345 12bbb33 678tt; 345, 678"
+			"cdefg 345 12bbb33 678tt; 345, 678",
+			"ofds002la021 410358; 002, 021, 410358",
+			"ksauokfsdornspvfuIUFk; ''",
+			"IOSdjaJS0akKna12 20Lo123938%% kauAn492; 123938, 492"
 	}, delimiter = ';')
 	void shouldReturnDigitSequencesLongerThanTwoTest(String input, String expected) {
 		String actual = Regex.digitSequence(input);
