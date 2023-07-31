dfdbeff5edc6b2d55e607c210a9d010949b7a7d6
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-31 12:56:24 +0200

    Add password requirements

--- chapter-04/src/main/java/PasswordValidator.java
+++ chapter-04/src/main/java/PasswordValidator.java
@@ -1,5 +1,13 @@
 public class PasswordValidator {
 
+	private static final int MIN_CHARS = 12;
+	private static final int MIN_LOWERCASE = 1;
+	private static final int MIN_UPPERCASE = 1;
+	private static final int MIN_SYMBOLS = 1;
+	private static final int MIN_DIGITS = 1;
+
+	private static final String SYMBOLS = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
+
 	public static boolean isValid(String password) {
 		return false;
 	}
