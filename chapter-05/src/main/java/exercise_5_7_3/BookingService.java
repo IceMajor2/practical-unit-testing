664f3b89ab1c4da1688b892480ceeecb4836e1e6
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-10 10:58:22 +0200

    Implement fetching a list and adding classrooms

--- chapter-05/src/main/java/exercise_5_7_3/BookingService.java
+++ chapter-05/src/main/java/exercise_5_7_3/BookingService.java
@@ -1,4 +1,17 @@
 package exercise_5_7_3;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class BookingService {
+
+	private Collection<Classroom> classrooms = new ArrayList<>();
+
+	public void addClassroom(Classroom classroom) {
+		this.classrooms.add(classroom);
+	}
+
+	public Collection<Classroom> getClassrooms() {
+		return this.classrooms;
+	}
 }
