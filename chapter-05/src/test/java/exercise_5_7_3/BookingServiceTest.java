1ce031f7656a2e6a4ec19fa38941817ae19f4b12
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-10 10:57:47 +0200

    Testing for a non-empty list of classrooms from booking service

--- chapter-05/src/test/java/exercise_5_7_3/BookingServiceTest.java
+++ chapter-05/src/test/java/exercise_5_7_3/BookingServiceTest.java
@@ -1,11 +1,25 @@
 package exercise_5_7_3;
 
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class BookingServiceTest {
 
 	private Classroom roomA1 = mock(Classroom.class, "A1");
 	private Classroom roomB2 = mock(Classroom.class, "B2");
 	private Classroom roomC3 = mock(Classroom.class, "C3");
+	private BookingService bookingService = new BookingService();
+
+	@Test
+	void shouldReturnAllClassrooms() {
+		bookingService.addClassroom(roomA1);
+		bookingService.addClassroom(roomB2);
+		bookingService.addClassroom(roomC3);
+
+		assertThat(bookingService.getClassrooms())
+				.containsExactlyInAnyOrder(roomA1, roomB2, roomC3);
+	}
 }
