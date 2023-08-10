c07478fdc50321f061aced51405be5cd92a77faf
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-10 11:45:04 +0200

    Testing for booking a classroom by name

--- chapter-05/src/test/java/exercise_5_7_3/BookingServiceTest.java
+++ chapter-05/src/test/java/exercise_5_7_3/BookingServiceTest.java
@@ -2,10 +2,12 @@ package exercise_5_7_3;
 
 import java.time.DayOfWeek;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class BookingServiceTest {
@@ -14,6 +16,13 @@ class BookingServiceTest {
 	private Classroom roomB2 = mock(Classroom.class, "B2");
 	private Classroom roomC3 = mock(Classroom.class, "C3");
 	private BookingService bookingService = new BookingService();
+
+	@BeforeEach
+	void setUp() {
+		when(roomA1.getName()).thenReturn("A1");
+		when(roomB2.getName()).thenReturn("B2");
+		when(roomC3.getName()).thenReturn("C3");
+	}
 
 	@Test
 	void shouldReturnAllClassrooms() {
@@ -42,6 +51,16 @@ class BookingServiceTest {
 
 		assertThat(bookingService.getAvailableClassrooms(DayOfWeek.FRIDAY, 10, 15))
 				.containsExactlyInAnyOrder(roomB2, roomC3);
+	}
 
+	@Test
+	void enableBookingOfClassroomByName() {
+		when(roomA1.isAvailable(DayOfWeek.SATURDAY, 15, 16))
+				.thenReturn(true);
+
+		bookingService.addClassroom(roomA1);
+		bookingService.book("A1", DayOfWeek.SATURDAY, 15, 16);
+
+		verify(roomA1).book(DayOfWeek.SATURDAY, 15, 16);
 	}
 }
