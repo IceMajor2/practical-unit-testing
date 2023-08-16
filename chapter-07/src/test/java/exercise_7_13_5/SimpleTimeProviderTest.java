8720a00bbe9fcd6f8958e34aa34848f781f10b86
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-16 15:58:31 +0200

    Add 'calendar' to constructor of SimpleTimeProvider

--- chapter-07/src/test/java/exercise_7_13_5/SimpleTimeProviderTest.java
+++ chapter-07/src/test/java/exercise_7_13_5/SimpleTimeProviderTest.java
@@ -1,21 +1,18 @@
 package exercise_7_13_5;
 
 import java.util.Calendar;
-import java.util.Date;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class SimpleTimeProviderTest {
 
-	private TimeProvider timeProvider = new SimpleTimeProvider();
-
 	private Calendar calendar = mock(Calendar.class);
+	private TimeProvider timeProvider = new SimpleTimeProvider(calendar);
 
 	private static int[] morningHours() {
 		return new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
