cf59f4a5647c54734bbfd37da880e860d42dc73e
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-16 15:58:51 +0200

    Implement TimeProvider interface

---
+++ chapter-07/src/main/java/exercise_7_13_5/SimpleTimeProvider.java
@@ -0,0 +1,22 @@
+package exercise_7_13_5;
+
+import java.util.Calendar;
+
+public class SimpleTimeProvider implements TimeProvider {
+
+	private Calendar calendar;
+
+	public SimpleTimeProvider() {
+		this.calendar = Calendar.getInstance();
+	}
+
+	public SimpleTimeProvider(Calendar calendar) {
+		this.calendar = calendar;
+	}
+
+	@Override
+	public boolean isMorning() {
+		System.out.println(calendar.get(Calendar.HOUR_OF_DAY));
+		return calendar.get(Calendar.HOUR_OF_DAY) < 12;
+	}
+}
