7edf596bd741f16bd0b8c98097c08f87cc7b51e3
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-16 16:00:59 +0200

    Expect false on SimpleTimeProvider.isMorning when the hour is an afternoon one

--- chapter-07/src/test/java/exercise_7_13_5/SimpleTimeProviderTest.java
+++ chapter-07/src/test/java/exercise_7_13_5/SimpleTimeProviderTest.java
@@ -12,10 +12,15 @@ import static org.mockito.Mockito.when;
 public class SimpleTimeProviderTest {
 
 	private Calendar calendar = mock(Calendar.class);
+
 	private TimeProvider timeProvider = new SimpleTimeProvider(calendar);
 
 	private static int[] morningHours() {
 		return new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+	}
+
+	private static int[] afternoonHours() {
+		return new int[] { 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 };
 	}
 
 	@ParameterizedTest
@@ -24,5 +29,13 @@ public class SimpleTimeProviderTest {
 		when(calendar.get(Calendar.HOUR_OF_DAY)).thenReturn(morningHour);
 
 		assertThat(timeProvider.isMorning()).isTrue();
+	}
+
+	@ParameterizedTest
+	@MethodSource("afternoonHours")
+	void givenAfternoonHour_whenIsMorning_thenReturnFalseTest(int afternoonHour) {
+		when(calendar.get(Calendar.HOUR_OF_DAY)).thenReturn(afternoonHour);
+
+		assertThat(timeProvider.isMorning()).isFalse();
 	}
 }
