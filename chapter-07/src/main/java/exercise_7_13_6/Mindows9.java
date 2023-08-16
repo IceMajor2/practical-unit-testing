473ea9e2032192f81ec78176014e59356c0380ea
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-16 16:45:17 +0200

    Implement Mindows9 OS :)

--- chapter-07/src/main/java/exercise_7_13_6/Mindows9.java
+++ chapter-07/src/main/java/exercise_7_13_6/Mindows9.java
@@ -1,4 +1,11 @@
 package exercise_7_13_6;
 
 public class Mindows9 extends OperatingSystem {
+
+	public Mindows9() {
+		super();
+		setNbOfBits(128);
+		setReleaseYear(2019);
+		setVersion(9);
+	}
 }
