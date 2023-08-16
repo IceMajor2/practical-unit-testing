95ccf3fb8be7aa7c3b61aaec603d27f25a55e8ec
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-16 15:07:18 +0200

    Add new test

--- chapter-07/src/test/java/exercise_7_13_4/HelpDeskTest.java
+++ chapter-07/src/test/java/exercise_7_13_4/HelpDeskTest.java
@@ -5,24 +5,32 @@ import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentMatchers;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class HelpDeskTest {
 
 
-
 	private TimeProvider timeProvider = mock(TimeProvider.class);
+
 	private HelpDesk helpDesk = new HelpDesk(timeProvider);
+
 	private Issue issue = mock(Issue.class);
 
 	static Stream<Integer> workingHoursOnFriday() {
-		return Stream.of(
-				0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-				10, 11, 12, 13, 14, 15, 16, 17
-		);
+		return Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+				10, 11, 12, 13, 14, 15, 16, 17);
+	}
+
+	static Stream<Integer> workingHours() {
+		return Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+				10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+				20, 21, 22, 23);
 	}
 
 	@ParameterizedTest
@@ -32,5 +40,17 @@ class HelpDeskTest {
 		when(timeProvider.getHour()).thenReturn(workingHour);
 
 		assertThat(helpDesk.willHandleIssue(issue)).isTrue();
+	}
+
+	@ParameterizedTest
+	// '2' is Monday, '3' is Tuesday, '4' is Wednesday, '5' is Thursday
+	@ValueSource(ints = {2, 3, 4, 5})
+	void handlingIssueOnBusinessDaysTest(int day) {
+		workingHours().forEach(hour -> {
+			when(timeProvider.getHour()).thenReturn(hour);
+			when(timeProvider.getDay()).thenReturn(day);
+
+			assertThat(helpDesk.willHandleIssue(issue)).isTrue();
+		});
 	}
 }
