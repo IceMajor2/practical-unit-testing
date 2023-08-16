f0c05b11ec12089c3747d2167ef2ad2d05b8e6b4
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-16 13:44:46 +0200

    Redesign code to be more testeable. Specifically, add a TimeProvider wrapper-class

--- chapter-07/src/main/java/exercise_7_13_4/HelpDesk.java
+++ chapter-07/src/main/java/exercise_7_13_4/HelpDesk.java
@@ -3,17 +3,17 @@ package exercise_7_13_4;
 import java.util.Calendar;
 
 public class HelpDesk {
-	
+
+	private TimeProvider timeProvider;
 	public final static int EOB_HOUR = 17;
 
 	public boolean willHandleIssue(Issue issue) {
-		Calendar cal = Calendar.getInstance();
-		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+		int dayOfWeek = timeProvider.getDay();
 		if (Calendar.SUNDAY == dayOfWeek || Calendar.SATURDAY == dayOfWeek) {
 			return false;
 		}
 		if (Calendar.FRIDAY == dayOfWeek) {
-			int hour = cal.get(Calendar.HOUR_OF_DAY);
+			int hour = timeProvider.getHour();
 			if (hour > EOB_HOUR) {
 				return false;
 			}
---
+++ chapter-07/src/main/java/exercise_7_13_4/TimeProvider.java
@@ -0,0 +1,7 @@
+package exercise_7_13_4;
+
+public interface TimeProvider {
+
+	int getDay();
+	int getHour();
+}
