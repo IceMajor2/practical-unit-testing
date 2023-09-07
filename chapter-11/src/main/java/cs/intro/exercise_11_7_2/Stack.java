f86fbf0c5b385fc381961368fbf6fe1557a0e767
IceMajor <icemajorr2@gmail.com>
2023-09-07 10:37:11 +0200

    Write some intro test-templates that focus on expected behaviour

---
+++ chapter-11/src/main/java/cs/intro/exercise_11_7_2/Stack.java
@@ -0,0 +1,17 @@
+package cs.intro.exercise_11_7_2;
+
+public class Stack<T> {
+
+    T pop() {
+        return null;
+    }
+
+    void push(T object) {
+
+    }
+
+    int size() {
+        return 0;
+    }
+
+}
---
+++ chapter-11/src/test/java/cs/intro/exercise_11_7_2/StackTest.java
@@ -0,0 +1,33 @@
+package cs.intro.exercise_11_7_2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StackTest {
+
+    @Test
+    void shouldReturnLastItemOnPop() {
+
+    }
+
+    @Test
+    void shouldThrowExceptionOnPopWhenEmpty() {
+
+    }
+
+    @Test
+    void shouldDecreaseSizeOnPop() {
+        
+    }
+
+    @Test
+    void shouldIncreaseSizeOnPush() {
+
+    }
+
+    @Test
+    void shouldThrowExceptionOnPushingNullObject() {
+
+    }
+}
