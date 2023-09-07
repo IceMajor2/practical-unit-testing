a11bb6e491cbff882d97713a6b085dc656f0f54a
IceMajor <icemajorr2@gmail.com>
2023-09-07 12:15:05 +0200

    Basic push and pop implementation to past the first test

--- chapter-11/src/main/java/cs/intro/exercise_11_7_2/Stack.java
+++ chapter-11/src/main/java/cs/intro/exercise_11_7_2/Stack.java
@@ -1,13 +1,19 @@
 package cs.intro.exercise_11_7_2;
 
+import linkedlist.LinkedList;
+
 public class Stack<T> {
 
+    private LinkedList<T> linkedList = new LinkedList<>();
+
     T pop() {
-        return null;
+        T toReturn = linkedList.getLast();
+        linkedList.removeLast();
+        return toReturn;
     }
 
     void push(T object) {
-
+        linkedList.add(object);
     }
 
     int size() {
